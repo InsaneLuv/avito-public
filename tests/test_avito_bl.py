28bc4328c5b69706e332585ac8d9ff23bfb06a39
InsaneLuv <69699689+InsaneLuv@users.noreply.github.com>
2026-02-11 10:00:30 +0400

    Тесты bl

---
+++ tests/test_avito_bl.py
@@ -0,0 +1,11 @@
+import pytest
+
+from app.models.avito import ChatsPayloadFilter, ChatsResponse, ChatTypeEnum, SendMessage, SendMessagePayload
+from app.services.avito import Avito, AvitoBL, AvitoModels
+
+
+@pytest.mark.asyncio
+class TestAvitoBL:
+
+    async def test_avito_bl(self, avito_bl: AvitoBL):
+        print(await avito_bl.meta())
