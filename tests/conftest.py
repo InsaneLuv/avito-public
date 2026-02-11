d2ccc38347064904ea3650cc6306adc7b1dd19c2
InsaneLuv <69699689+InsaneLuv@users.noreply.github.com>
2026-02-11 09:59:49 +0400

    Тесты для промпт ридера

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
+from app.prompts.read import PromptReader
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from typing import AsyncGenerator
 import pytest
 
-from app.services.avito import AvitoModels
+from app.services.avito import Avito, AvitoBL
 
 import dotenv
 from openai import AsyncOpenAI
@@ -39,7 +41,7 @@ def openai_api_token():
 async def httpx_client_proxied() -> AsyncGenerator[AsyncClient, None]:
     """Создаем HTTP клиент с прокси для всей сессии."""
     async with AsyncClient(
-            proxy=f"http://{SQUID_PROXY_USER}:{SQUID_PROXY_PASSWORD}@{SQUID_PROXY_HOST}:{SQUID_PROXY_PORT}") as client:
+            proxy=f"http://{SQUID_PROXY_USER}:{SQUID_PROXY_PASSWORD}@{SQUID_PROXY_HOST}:{SQUID_PROXY_PORT}", timeout=600) as client:
         yield client
 
 
@@ -47,13 +49,30 @@ async def httpx_client_proxied() -> AsyncGenerator[AsyncClient, None]:
 async def avito(client_id, client_secret):
     """Создаем клиент Avito для всей сессии тестов."""
     # Важно: создаем клиент и используем его в том же event loop
-    client = AvitoModels(client_id, client_secret)
+    client = Avito(client_id, client_secret)
     await client.get_user_data()
     return client
-
 
 
 @pytest.fixture(scope="session")
 async def openai(openai_api_token, httpx_client_proxied):
     """Создаем клиент OpenAI для всей сессии тестов."""
-    return AsyncOpenAI(api_key=openai_api_token, http_client=httpx_client_proxied)
+    return AsyncOpenAI(api_key=openai_api_token, http_client=httpx_client_proxied, timeout=600)
+
+@pytest.fixture(scope="session")
+async def prompts_reader():
+    client = PromptReader()
+    return client
+
+@pytest.fixture(scope="session")
+async def prompt(prompts_reader):
+    return await prompts_reader.read_text("text.md")
+
+
+
+@pytest.fixture(scope="function")
+async def avito_bl(avito, openai, prompt):
+    client = AvitoBL(avito, openai, prompt)
+    return client
+
+
