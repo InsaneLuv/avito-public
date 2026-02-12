0220cd1a2e6ff2c990ec4cce1406626e26cac664
InsaneLuv <69699689+InsaneLuv@users.noreply.github.com>
2026-02-12 19:06:06 +0400

    Обновлённый конфиг

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from contextlib import asynccontextmanager
 
+from dishka import make_async_container
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from app.api.routes.health import router
 from app.core.config import get_app_settings
+from app.core.providers import ConfigProvider
 
 
 @asynccontextmanager
@@ -12,18 +14,31 @@ async def lifespan(app: FastAPI):
     yield
 
 
+def setup_dependencies(app: FastAPI):
+    container = make_async_container(
+        ConfigProvider("prod"),
+    )
+    setup_dishka(container, app)
+    return container
+
+
 def get_application() -> FastAPI:
     settings = get_app_settings()
 
-    application = FastAPI(**settings.fastapi_kwargs, lifespan=lifespan)
+    application = FastAPI(**settings.app.fastapi_kwargs, lifespan=lifespan)
+    setup_dependencies(application)
+
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.allowed_hosts,
+        allow_origins=settings.app.allowed_hosts,
         allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    application.include_router(router, prefix=settings.api_prefix)
+    from app.api.routes.health import router
+    application.include_router(router, prefix=settings.app.api_prefix)
+    from app.api.routes.prompt import router
+    application.include_router(router, prefix=settings.app.api_prefix)
     return application
 
 
